59cd4933218e3d4bbf939e2b74acc088bb49e8cf
Pascal Roth <roth.pascal@outlook.de>
2025-08-06 16:27:26 +0200

    formatter

--- exts/nav_tasks/nav_tasks/mdp/observations/height_scan_observations.py
+++ exts/nav_tasks/nav_tasks/mdp/observations/height_scan_observations.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025, The Nav-Suite Project Developers (https://github.com/leggedrobotics/nav-suite/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from __future__ import annotations
 
 import math
