c74a72402d18203686aa8354467790eaa29ed417
Pascal Roth <roth.pascal@outlook.de>
2025-08-06 09:27:19 +0200

    add height scan obs terms

--- exts/nav_tasks/nav_tasks/mdp/observations/__init__.py
+++ exts/nav_tasks/nav_tasks/mdp/observations/__init__.py
@@ -6,5 +6,6 @@
 from .camera_observations import *  # noqa: F401, F403
 from .camera_observations_cfg import *  # noqa: F401, F403
 from .depth_embedding import *  # noqa: F401, F403
+from .height_scan_observations import *  # noqa: F401, F403
 from .observations import *  # noqa: F401, F403
 from .observations_cfg import *  # noqa: F401, F403
